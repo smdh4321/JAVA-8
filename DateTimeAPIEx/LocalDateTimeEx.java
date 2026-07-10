eed4f1a5440dd586c2bf86115710109b5b4e66f2
Sunkara Mohammed Hussain <smdh4321@gmail.com>
2026-07-10 11:49:34 +0530

    LocalDateTime

---
+++ DateTimeAPIEx/LocalDateTimeEx.java
@@ -0,0 +1,12 @@
+package DateTimeAPIEx;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTimeEx {
+    public static void main(String[] args) {
+        LocalDateTime today = LocalDateTime.now();
+        System.out.println(today);
+
+        //same methods as LocalDate/LocalTime
+    }
+}
